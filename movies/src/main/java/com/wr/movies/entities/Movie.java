fe0a0d92b8c6ee247f616c7600e9b282bd009e3a
weslyramalho <weslyramalho@yahoo.com.br>
2022-05-30 15:59:21 -0300

    Criado class Movie

--- movies/src/main/java/com/wr/movies/entities/Movie.java
+++ movies/src/main/java/com/wr/movies/entities/Movie.java
@@ -4,5 +4,47 @@ public class Movie {
     private Long id;
     private String titulo;
     private String comentario;
+    private User user;
 
+    public Movie() {
+    }
+
+    public Movie(Long id, String titulo, String comentario, User user) {
+        this.id = id;
+        this.titulo = titulo;
+        this.comentario = comentario;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
