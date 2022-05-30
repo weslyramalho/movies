ad3456741b3a5920ab9adcfa13c14c927d46c2cf
weslyramalho <weslyramalho@yahoo.com.br>
2022-05-30 14:45:21 -0300

    modificando estrutura

--- movies/src/main/java/com/wr/movies/MoviesApplication.java
+++ movies/src/main/java/com/wr/movies/MoviesApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.wr.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MoviesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MoviesApplication.class, args);
 	}
 
 }
