7e1d46c858eacf66580d03a6bcb597e3f9d16a52
weslyramalho <weslyramalho@yahoo.com.br>
2022-06-03 16:45:28 -0300

    jms

--- movies/src/main/java/com/wr/movies/config/JMSConfiguration.java
+++ movies/src/main/java/com/wr/movies/config/JMSConfiguration.java
@@ -1,4 +1,47 @@
 package com.wr.movies.config;
 
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJcaListenerContainerFactory;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+
+import javax.jms.ConnectionFactory;
+import java.awt.*;
+
+@Configuration
 public class JMSConfiguration {
+
+    //para filas
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                                    DefaultJmsListenerContainerFactoryConfigurer configurer,
+                                                    JmsErrorHandler jmsErrorHandler){
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setErrorHandler(jmsErrorHandler);
+        configurer.configure((factory, connectionFactory));
+        return factory;
+    }
+
+    //para topicos
+    @Bean
+    public JmsListenerContainerFactory<?> topicListenerFactory(ConnectionFactory connectionFactory,
+                                                               DefaultJmsListenerContainerFactoryConfigurer configurer,){
+       DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+       factory.setPubSubDomain(true);
+       configurer.configure(factory, connectionFactory);
+       return factory;
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdProperTyName("_type");
+        return converter;
+    }
 }
